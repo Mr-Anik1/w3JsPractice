33d8ea7504075c223dd6e1cde2398a20ccbed300
Anik <anik2002bro@gmail.com>
2022-12-08 23:37:50 +0600

    2nd problem | Dec 8 2022 ~ 11:37 PM

--- js-basic/2.current-date.js
+++ js-basic/2.current-date.js
@@ -3,3 +3,15 @@
 Expected Output :
 mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
 */
+let today = new Date();
+
+let mm = today.getMonth() + 1;
+mm = mm < 10 ? `0${mm}` : `${mm}`;
+let dd = today.getDate();
+dd = dd < 10 ? `0${dd}` : `${dd}`;
+let yy = today.getFullYear();
+
+console.log(`${mm}-${dd}-${yy}`);
+console.log(`${mm}/${dd}/${yy}`);
+console.log(`${dd}-${mm}-${yy}`);
+console.log(`${dd}/${mm}/${yy}`);
